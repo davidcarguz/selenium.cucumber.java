846bacee9976f76b6604a1edccff638db26f93f5
David Cardenas Guzman <davidcarguz@gmail.com>
2020-06-24 11:52:23 -0500

    added configuration file, class and methods to set the properties from the file, added support to chrome, firefox and edge according to the value set in properties file.

--- src/test/java/core/WebDriverFactory.java
+++ src/test/java/core/WebDriverFactory.java
@@ -3,6 +3,8 @@ package core;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.concurrent.TimeUnit;
@@ -16,11 +18,14 @@ public class WebDriverFactory {
      * sets the implicit wait, maximize window and the url.
      */
     public void startDriver(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://opensource-demo.orangehrmlive.com/");
+        driver = setDriver(System.getProperty("browser"));
+        if(driver != null) {
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Integer.parseInt(System.getProperty("wait.implicit")), TimeUnit.SECONDS);
+            driver.get(System.getProperty("url"));
+        }else{
+            throw new Error("Driver could not be created.");
+        }
     }
 
     /**
@@ -35,5 +40,29 @@ public class WebDriverFactory {
      */
     public void closeDriver(){
         driver.quit();
+    }
+
+    /**
+     * Instantiates the driver with the desired browser.
+     * @param browser Browser where the test are going to be executed.
+     * @return Webdriver instantiated with defined browser.
+     */
+    public WebDriver setDriver(String browser){
+        WebDriver driver;
+        if(browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }else
+            if(browser.equalsIgnoreCase("firefox")){
+                WebDriverManager.firefoxdriver().arch32().setup();
+                driver = new FirefoxDriver();
+        }else
+            if(browser.equalsIgnoreCase("edge")){
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+            }else{
+                driver = null;
+            }
+        return driver;
     }
 }
