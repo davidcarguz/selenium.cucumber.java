b1ef7ffdc60bae63e7db1d3c1b4833eb2b88f463
David Cardenas Guzman <davidcarguz@gmail.com>
2020-08-18 20:59:32 -0500

    added Login javadoc.

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+/**
+ * Login methods.
+ */
 public class Login extends WebDriverFactory {
 
     @FindBy(how = How.ID, using = "txtUsername")
