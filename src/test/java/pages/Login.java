83bb98ee729db502c660b45298ab707b83f50966
David Cardenas Guzman <davidcarguz@gmail.com>
2020-08-18 21:30:34 -0500

    modified Login javadoc.

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 /**
- * Login methods.
+ * Login ui methods.
  */
 public class Login extends WebDriverFactory {
 
